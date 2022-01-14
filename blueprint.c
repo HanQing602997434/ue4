d4f774db0e78f91ae18feb08ec9856939db0a5ab
hq <602997434@qq.com>
2022-01-14 03:19:16 -0800

    蓝图

---
+++ blueprint.c
@@ -0,0 +1,21 @@
+
+// 蓝图
+/*
+    AIController
+        ai控制器
+
+    Behavior Tree
+        行为树
+
+    Blackbord
+        黑板
+
+    Decorator
+        装饰
+
+    Service
+        服务
+
+    AI流程
+        创建AI -》 创建AIController并设置为AI的AI Controller Class -》 RunBehaviorTree -》 设置行为树 -》 执行各种行为 -》 Sequence序列 -》 Selector选择器
+*/
