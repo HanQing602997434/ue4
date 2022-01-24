7c949501a9ea2ebe8fcc5dd1ba8acde3584f72d7
hq <602997434@qq.com>
2022-01-24 02:02:38 -0800

    UMG

---
+++ umg.c
@@ -0,0 +1,31 @@
+
+// UMG
+/*
+    CreateUIWidget
+        创建UI，class指定要创建的UI
+
+    AddtoViewport
+        添加到玩家屏幕
+
+    RemovefromParent
+        从父类移除UI
+
+    FlipFlop
+        触发器，检测同一按键的多次输入，第一次按键为A，第二次按键为B
+
+    Canvas Panel Designer面板
+        Tool Tip Text
+            当用户用鼠标悬停在控件上时显示的工具提示文本
+
+        is Enabled
+            是否启用
+
+        Visibility
+            可视化
+
+        Render Opacity
+            渲染透明度
+
+        Tool Tip Widget
+            当用户用鼠标悬停在控件上时显示UI
+*/
