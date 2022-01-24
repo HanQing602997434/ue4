cf864fc3f4bff379b296ba298a6d9c024401173f
hq <602997434@qq.com>
2022-01-24 02:41:12 -0800

    UMG

--- umg.c
+++ umg.c
@@ -24,8 +24,56 @@
             可视化
 
         Render Opacity
-            渲染透明度
+            渲染不透明度
 
         Tool Tip Widget
             当用户用鼠标悬停在控件上时显示UI
+
+    Text Block
+        Slot
+            控件属于那个画布的插槽
+
+            Anchors
+                锚点，控件显示的位置
+
+            Alignment
+                控制对齐
+
+            Size to Content
+                自适应大小
+
+        Content
+            内容
+
+        Appearance
+            外观
+
+            Color and Opacity
+                颜色和不透明度
+
+        Font
+            字体
+
+            Font Family
+                字体系统
+
+            Typeface
+                二级字体
+
+            Size
+                字体大小
+
+            Font Material
+                字体材质
+
+            Outline Settings
+                边框设置
+
+                Outline Size
+                    边框大小
+
+                Separate Fill Alpha
+                    边框分离
+
+                
 */
