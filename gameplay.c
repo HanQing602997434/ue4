6642a998a65ecebae3243156cd6fbb92aee73b6b
hq <602997434@qq.com>
2022-06-12 19:37:18 -0700

    GamePlay框架

--- gameplay.c
+++ gameplay.c
@@ -14,4 +14,16 @@
         为Actor提供能力
         能力一般与业务逻辑无关
         能力是跨游戏的
+
+    ALevelScriptActor-关卡蓝图
+        一个特殊的默认脚本Actor:ALevelScriptActor
+        正经项目-大部分情况都不适合
+        适合Demo，新手村
+        适合一些关卡本身的触发机械互动
+
+    APawn
+        可被控制
+        逻辑足够复杂MVC
+        肉体
+        人形角色
 */
